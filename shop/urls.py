2233c89c68f947e1543037db8741346c20ed5f32
willyowi <wilsonowino1@gmail.com>
2020-05-20 12:24:25 +0300

    updates

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 app_name = 'shop'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    url(r'^shopnow/',views.shop_now, name='shop_now'),
     path('<slug:category_slug>/', views.product_list,name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail,name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -11,8 +11,16 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request,'shop/product/home-page.html',{'category': category,'categories': categories,'products': products})
+    return render(request,'shop/product/index.html',{'category': category,'categories': categories,'products': products})
+def shop_now(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
 
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,'shop/product/shop.html',{'category': category,'categories': categories,'products': products})
 def product_detail(request, id, slug):
     product = get_object_or_404(Product,id=id,slug=slug,available=True)
         
